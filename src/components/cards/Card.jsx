1df454605cd2b2df493369d463930745ed639955
aadhar2003 <68272092+aadhar2003@users.noreply.github.com>
2023-08-16 19:49:01 +0530

    commmit

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -31,40 +31,21 @@ const Card = (props) => {
   };
   const title = {
     color: props.projectTitleColor,
-    // margin:'4px 0px',
-    // fontSize:'20px',
-    // fontWeight:'bold',
+ 
   };
   const location = {
     color: props.locationColor,
   };
   const about = {
     color: props.aboutColor,
-    // marginTop:'30px',
-    // marginBottom:'20px',
-    // width:'400px'
+ 
   };
   const tech = {
     color: props.techColor,
     borderColor: props.border,
-    // border:'1px solid',
-    // borderRadius:'9px',
-
-    // marginRight:'5px',
-
-    // paddingBottom:'3px',
-    // paddingLeft:'4px',
-    // paddingRight:'4px',
+ 
   };
-  // const imgStyle={
-  //   width: '100%',
-  //   objectFit:'fill',
-  //   objectPosition:'center',
-  //   position: 'relative',
-  //   zIndex: '2',
-  //   right: '13%',
-  //   top: '7%'
-  // }
+ 
 
   return (
     <div style={mystyle} key={props.id} className="P-card">
@@ -86,11 +67,6 @@ const Card = (props) => {
             );
           })}
         </div>
-        {/* {props.github && (
-          <div>
-           <a href={url} target="_blank"> <img src={github} /></a>
-          </div>
-        )} */}
       </div>
       <div className="card-img">
         <img src={deskImg} alt="" className="CM-desk" onMouseOver={changeImg} onMouseLeave={resetImg}/>
--- src/components/cards/data.js
+++ src/components/cards/data.js
@@ -16,10 +16,10 @@ const data = [
   {
     id:1,
     sno: "PROJECT 1",
-    projectTitle: "Cable Driven Parallel Robot",
+    projectTitle: "Smart Home Automation System",
     location: "Summer Project Intern at eYantra IIT Bombay",
     about:
-      "The bot utilizes cables or strings to manipulate a payload, offering high precision and flexibility",
+      "Developed an advanced home automation system using IoT technology for seamless control and monitoring of various home devices.",
     techStack: ["Python", "Open CV", "ESP32", "Kinematics", "CAD"],
     backgroundColor:'#081E48',
     snoColor:'#246AFD',
@@ -31,16 +31,17 @@ const data = [
     img:img1,
     img2:img5,
     img3:img9,
+    Link:'projectlink'
     
   },
   {
     id:2,
     sno: "PROJECT 2",
-    projectTitle: "IN-PIPE BOT",
+    projectTitle: "E-commerce Platform Enhancement",
     location: "Project for Robotix IITH",
     about:
-      "The bot detects cracks in the pipes while traversing through it.",
-    techStack: ["Python", "Open CV", "Image Processing", "Neutral Networks"],
+      "Contributed to enhancing a large-scale e-commerce platform, implementing new features and optimizing the user experience.",
+    techStack: ["JavaScript", "React", "Node js", "MongoDB"],
     backgroundColor:'#084848',
     snoColor:'#1AAEAE',
     projectTitleColor:'#FFFFFF',
@@ -51,14 +52,15 @@ const data = [
     img:img2,
     img2:img6,
     img3:img11,
+    Link:'projectlink'
   },
   {id:3,
     sno: "PROJECT 3",
-    projectTitle: "Heart Rate Monitor using TCRT",
+    projectTitle: "Personal Finance Tracker App",
     location: "For course EE2301",
     about:
-      "The device uses the amount of IR rays reflected from ones finger to estimate the rate of blood flow and hence, the heart rate",
-    techStack: ["Analog Filters", "Electronics", "Arduino Microcontrollers"],
+      "Created a user-friendly finance tracker app to help users manage their expenses, track budgets, and visualize spending patterns.",
+    techStack: ["Flutter", "Dart", "SQLite"],
     backgroundColor:'#482E08',
     snoColor:'#FDC024',
     projectTitleColor:'#FFFFFF',
@@ -69,15 +71,16 @@ const data = [
     img:img3,
     img2:img7,
     img3:img10,
+    Link:'projectlink'
   },
   {
     id:4,
     sno: "PROJECT 4",
-    projectTitle: "Thermometer using PT100",
-    location: "For course EE2301",
+    projectTitle: "SmartHome Energy Management System",
+    location: "Seattle, Washington, USA",
     about:
-      "Mapping the resistance of PT100 sensor to corresponding temperature using regression.",
-    techStack: ["Polynomial Regression", "Machine Learning", "Microcontrollers"],
+      "Designed and developed a SmartHome Energy Management System to optimize energy usage and reduce costs in residential environments.",
+    techStack: ["MQTT", "React", "Django"],
     backgroundColor:'#082948',
     snoColor:'#24BCFD',
     projectTitleColor:'#FFFFFF',
@@ -88,6 +91,7 @@ const data = [
     img:img4,
     github:true,
     img2:img8,
-    img3:img12,  }
+    img3:img12,
+    Link:'projectlink'  }
 ];
 export default data;
